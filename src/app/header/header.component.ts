bd7394ecf5088a5c5f178abdc5c47a3e9aa386bd
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2018-10-05 04:49:27 +0000

    Add onscroll event on header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
+import { environment } from '../../environments/environment';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  readonly headerHeight: number = environment.headerHeight;
+  colorHeader: boolean = false;
 
   constructor() {}
+
+  onWindowScroll($event: Event) {
+    this.colorHeader = window.pageYOffset + this.headerHeight >= window.innerHeight;
+  }
 }
+
